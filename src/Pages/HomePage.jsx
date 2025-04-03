2eb7483d1baf7bf92a3a66b01396c27e06c1be50
Javier <javier.lombar2@gmail.com>
2025-04-03 16:42:42 +0200

    commit de prueba

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+return (
+    <div className="container mt-5">
+        <div className="row">
+            <div className="col text-center">
+                <h1 className="display-4">Welcome to Tech News</h1>
+                <p className="lead">Stay updated with the latest tech trends and news.</p>
+                <button className="btn btn-primary btn-lg">Learn More</button>
+            </div>
+        </div>
+    </div>
+)
 }
