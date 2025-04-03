1fac8628421ed07ccb57f37d85f21b739d1b5fbb
Javier <javier.lombar2@gmail.com>
2025-04-03 17:02:40 +0200

    estructura de proyecto

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,14 +2,7 @@ import React from 'react'
 
 export const HomePage = () => {
 return (
-    <div className="container mt-5">
-        <div className="row">
-            <div className="col text-center">
-                <h1 className="display-4">Welcome to Tech News</h1>
-                <p className="lead">Stay updated with the latest tech trends and news.</p>
-                <button className="btn btn-primary btn-lg">Learn More</button>
-            </div>
-        </div>
-    </div>
+   <>
+   </>
 )
 }
